b72585bcbdbb662c8235709215e9ae0b4a64a8f0
JorPan <jbpanasewicz@gmail.com>
2021-04-27 11:24:03 -0600

    login page

---
+++ src/pages/login.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useState, useContext, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import FirebaseContext from "../context/firebase";
+
+export default function Login() {
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState("");
+  const isInvalid = password === "" || emailAddress === "";
+
+  const handleLogin = () => {};
+
+  useEffect(() => {
+    document.title = "Login - Instafam";
+  }, []);
+
+  return <p>Log In Page</p>;
+}
